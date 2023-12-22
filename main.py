dcdad520d794da295b0a4aa3471443b62c031f33
Alexey <alexmail121212@gmail.com>
2023-12-22 19:50:44 +0300

    first commint

---
+++ main.py
@@ -0,0 +1,21 @@
+#print(5,8,6);
+
+'''
+n=5
+
+
+n1="66543"
+
+
+print(type(n),type(n));
+'''
+a=5
+b=3.33
+c="333\"";
+print(f"{a},{b},{c}");
+print("{},{},{}".format(a,b,c));
+
+a = int(input())
+b= input("Enter second number")
+print (a,b)
+print(int(a)+int(b))
