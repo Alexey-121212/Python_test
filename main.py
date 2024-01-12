74956e5e1f792fe17e47e5b291e1af946696d089
Alexey <alexmail121212@gmail.com>
2024-01-12 13:25:27 +0300

    dz4

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ n1="66543"
 
 
 print(type(n),type(n));
-'''
+
 a=5
 b=3.33
 c="333\"";
@@ -19,3 +19,39 @@ a = int(input())
 b= input("Enter second number")
 print (a,b)
 print(int(a)+int(b))
+
+
+a = 1.23432432
+b = 3.24142142421
+
+print (round(a*b,3))
+#print (b)
+
+
+a = 1 <2 <3 <3
+
+print (a)
+
+a=33
+if a ==1 :
+    print(1)
+elif a == 33:
+    print(33)
+else:
+    print("else")
+
+
+for i in range(10) :
+    print (i)
+
+
+a="qwerty"
+for i in a:
+    print (a, "    ", i)
+
+    '''
+k=""
+for i in range(5):
+    for j in range(5):
+        k+="*"
+    print (k)
