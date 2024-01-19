985585b7fbf2496b017940cfa2349891f9d115d5
Alexey <alexmail121212@gmail.com>
2024-01-19 17:33:33 +0300

    5-1 bug

--- dz5-1.py
+++ dz5-1.py
@@ -17,8 +17,30 @@
 
 # 3 5
 
-#var1 = '5 4' 
-#var2 = '1 3 5 7 9' 
-#var3 = '2 3 4 5' 
+var1 = '5 4' 
+var2 = '1 3 5 7 9' 
+var3 = '2 3 4 5' 
 
 # Введите ваше решение ниже
+
+list_1 = var1.split(' ')
+list_1 = [int(elt) for elt in list_1]
+
+list_2 = var2.split(' ')
+list_2 = [int(elt) for elt in list_2]
+
+list_3 = var3.split(' ')
+list_3 = [int(elt) for elt in list_3]
+
+
+list_fin = []
+
+for i in range(0, list_1[0]):
+    if (list_2[i] in list_3) & (list_2[i] not in list_fin):
+        list_fin.append(list_2[i])
+list_fin.sort()
+
+
+print (*list_fin)
+
+
