66b5c659ac26ae3abeb1ea89ef11fb84fbca90fa
Alexey <alexmail121212@gmail.com>
2024-01-31 00:45:54 +0300

    dz 7

--- dz6-1-1.py
+++ dz6-1-1.py
@@ -7,22 +7,25 @@
 
 
 list_1 = [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
-min_number = 0
-max_number = 10
-# На выходе:
+# min_number = 0
+# max_number = 10
+# # На выходе:
 
 
-# 1
-# 2
-# 3
-# 6
-# 7
-# 9
-# 10
-# 11
-# 13
-# 14
-# 16
-# 19
+# # 1
+# # 2
+# # 3
+# # 6
+# # 7
+# # 9
+# # 10
+# # 11
+# # 13
+# # 14
+# # 16
+# # 19
 
-print ([i for i in range(len(list_1)) if list_1[i]<=max_number & (list_1[i]>=min_number)])
+# # print ([i for i in range(len(list_1)) if list_1[i]<=max_number & (list_1[i]>=min_number)])
+
+list_1 = list(map(int, list_1.split()))
+print(list_1)
