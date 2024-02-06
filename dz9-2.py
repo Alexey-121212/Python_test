c68a89758eea8d2346ff48483c357047a547e9d9
Alexey <alexmail121212@gmail.com>
2024-02-07 01:35:56 +0300

    dz92

---
+++ dz9-2.py
@@ -0,0 +1,14 @@
+
+# Дан файл california_housing_train.csv.
+# Найти максимальное значение переменной "households" в зоне минимального значения переменной min_population и сохраните это значение в переменную max_households_in_min_population.
+# Используйте модуль pandas.
+
+
+
+import pandas as pd
+df = pd.read_csv('california_housing_train.csv')
+# max_households_in_min_population = df['households'][(df['median_house_value'] == df['median_house_value'].max())].min()
+# print(max_households_in_min_population)
+
+a = df['households'][(df['median_house_value'] == df['median_house_value'].max())].min()
+print(a)
